9ca57ebea7c39e7781608384ab9f87fd916a0ca9
cornicheli <gabrielcornide.pg@gmail.com>
2023-07-07 21:58:07 -0300

    agregado de scroll y filtros modificacion del json

--- src/asset/icon/index.js
+++ src/asset/icon/index.js
@@ -1,5 +1,6 @@
 import Email from './email.png'
 import PhoneIcon from './phoneIcon.png'
 import WhatsappIcon from './whatsappIco.png'
+import Option from './option.png'
 
-export { Email, PhoneIcon, WhatsappIcon }
+export { Email, PhoneIcon, WhatsappIcon, Option }
